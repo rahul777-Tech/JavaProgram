082a13598afa43a37453a7430f63b6ef18bfa81b
rahul777-Tech <rbkanthale@gmail.com.com>
2022-07-03 18:59:51 +0530

    Multiplcatontable

---
+++ src/ProgramOnNumber/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package ProgramOnNumber;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=3;
+		
+		for(int i=1; i<=10; i++) {
+			
+			System.out.println(n*i);
+			
+			
+		}
+
+	}
+
+}
